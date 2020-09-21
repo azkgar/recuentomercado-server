faf7458cb5c804fadd5cc1848caff8e9dadfc8d1
Azkary Garcia <agarcia@aparente.mx>
2020-09-21 15:33:11 -0500

    Create endpoint for updating menu items

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,6 +1,6 @@
 const Menu = require("../models/menu");
 
-function addMenu(req,res) {
+function addMenu(req, res) {
     const {title, url, order, active} = req.body;
     const menu = new Menu();
     menu.title = title;
@@ -21,7 +21,7 @@ function addMenu(req,res) {
     });
 }
 
-function getMenus(req,res) {
+function getMenus(req, res) {
     Menu.find().sort({order: "asc"}).exec((err, menusStored) => {
         if(err) {
             res.status(500).send({message: "Error del servidor."});
@@ -35,7 +35,25 @@ function getMenus(req,res) {
     });
 }
 
+function updateMenu(req, res) {
+    let menuData = req.body;
+    const params = req.params;
+
+    Menu.findByIdAndUpdate(params.id, menuData, (err, menuUpdate) => {
+        if(err) {
+            res.status(500).send({message: "Error del servidor."});
+        } else {
+            if(!menuUpdate) {
+                res.status(404).send({message: "No se encontró el menú."});
+            } else {
+                res.status(200).send({message: "Menú actualizado"});
+            }
+        }
+    });
+}
+
 module.exports = {
     addMenu,
-    getMenus
+    getMenus,
+    updateMenu
 }
--- routers/menu.js
+++ routers/menu.js
@@ -9,4 +9,6 @@ api.post("/add-menu", [md_auth.ensureAuth], MenuController.addMenu);
 
 api.get("/get-menus", MenuController.getMenus);
 
+api.put("/update-menu/:id", [md_auth.ensureAuth], MenuController.updateMenu);
+
 module.exports = api;

e4d6aa8024936016bddb8d99665bec2b89d0561d
Azkary Garcia <agarcia@aparente.mx>
2020-09-21 13:00:55 -0500

    Create model for newsletter

---
+++ models/newsletter.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const NewsletterSchema = Schema ({
+    email: {
+        type: String,
+        unique: true
+    },
+    name: String
+});
+
+module.exports = mongoose.model("Newsletter", NewsletterSchema);
